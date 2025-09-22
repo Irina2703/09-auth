5af9e7863eaab924ed983eb2e27c94a610b09d07
Kliucherova Irina <klucerovaira@gmail.com>
2025-09-22 16:58:06 +0200

    commit

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import css from "./Header.module.css";
 import Link from "next/link";
 import TagsMenu from "@/components/TagsMenu/TagsMenu";
+import AuthNavigation from "@/components/AuthNavigation/AuthNavigation";
 
 export default function Header() {
     return (
@@ -8,14 +11,19 @@ export default function Header() {
             <Link href="/" aria-label="Home">
                 NoteHub
             </Link>
+
             <nav aria-label="Main Navigation">
                 <ul className={css.navigation}>
                     <li>
                         <Link href="/">Home</Link>
                     </li>
+
                     <li>
                         <TagsMenu />
                     </li>
+
+                    {/* ↓ Здесь появятся кнопки Login/Sign-up или Profile/Logout */}
+                    <AuthNavigation />
                 </ul>
             </nav>
         </header>
